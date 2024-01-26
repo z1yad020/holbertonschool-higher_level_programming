7c0fe41acb1947f34fddc361b00a0ffe655cbfe5
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-01-26 02:25:21 -0800

    Remove at position

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    if n < 0:
+        return str
+
+    s = ""
+    for i in range(len(str)):
+        if i == n:
+            continue
+        s += str[i]
+    return s
+
+
+if __name__ == "__main__":
+    print(remove_char_at("Best School", 3))
+    print(remove_char_at("Chicago", 2))
+    print(remove_char_at("C is fun!", 0))
+    print(remove_char_at("School", 10))
+    print(remove_char_at("Python", -2))

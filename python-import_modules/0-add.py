d63bd34dee57799e8acaab5af1dfa1c06d2e9f4e
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-01-26 08:00:24 -0800

    Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-import add_0
+import add from add_0
+
 
 def func():
     a = 1
     b = 2
 
     print("{} + {} = {}".format(a, b, add(a, b)))
+
 
 if __name__ == "__main__":
     func()

7060951ad97475038ef2f68cbee186bf1a52f0ac
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-01-27 09:15:31 -0800

    5:17: E117 over-indented

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-                return my_list.copy()
+        return my_list.copy()
 
     x = my_list.copy()
     x[idx] = element

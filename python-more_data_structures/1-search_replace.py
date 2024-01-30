37d5859a6de513c04b1cc70e8379247c879f2d35
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-01-30 10:56:17 -0800

    Search and replace

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    t = my_list.copy()
+
+    while True:
+        try:
+            t[t.index(search)] = replace
+        except ValueError:
+            return t
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+    new_list = search_replace(my_list, 2, 89)
+
+    print(new_list)
+    print(my_list)

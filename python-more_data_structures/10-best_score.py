39a9a3fd6cf7048558f4d6c6e6b3fe674126f467
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-01-31 08:17:05 -0800

    Best student

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -5,7 +5,6 @@ def best_score(a_dictionary):
         return max(a_dictionary, key=a_dictionary.get)
 
 
-
 if __name__ == "__main__":
     a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
     best_key = best_score(a_dictionary)

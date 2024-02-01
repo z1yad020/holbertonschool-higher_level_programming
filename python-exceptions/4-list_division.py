8ee1578f5b031911d83678a09bbb4ce66db19e8b
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-01 05:13:48 -0800

    Divide a list

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    newl = []
+    for i in range(list_length):
+        try:
+            value = 0
+            value = my_list_1[i] / my_list_2[i]
+        except (TypeError, ValueError):
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            newl.append(value)
+    return newl
+
+
+if __name__ == "__main__":
+    my_l_1 = [10, 8, 4]
+    my_l_2 = [2, 4, 4]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)
+
+    print("--")
+
+    my_l_1 = [10, 8, 4, 4]
+    my_l_2 = [2, 0, "H", 2, 7]
+    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    print(result)

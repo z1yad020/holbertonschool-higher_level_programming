81960ea82b213739c56750d6bbf7415f38b95fa4
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-01 02:52:42 -0800

    declare i

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')
-        except:
+        except IndexError:
             print()
             return i
     print()

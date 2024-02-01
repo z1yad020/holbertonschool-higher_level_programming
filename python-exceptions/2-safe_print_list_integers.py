def560f0431d7afd718d920d0579973ce1b51fe6
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-01 04:17:53 -0800

    Print and count integers

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    c = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            c += 1
+        except IndexError:
+            raise
+        except Exception:
+            pass
+    print()
+    return c
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+
+    nb_print = safe_print_list_integers(my_list, 2)
+    print("nb_print: {:d}".format(nb_print))
+
+    my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+    nb_print = safe_print_list_integers(my_list, len(my_list))
+    print("nb_print: {:d}".format(nb_print))
+
+    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+    print("nb_print: {:d}".format(nb_print))

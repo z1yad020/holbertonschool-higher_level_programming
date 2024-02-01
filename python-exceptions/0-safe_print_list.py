05ae401a4c8689fda3498150371102b17e0b853a
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-01 02:57:16 -0800

    if statement

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0
+    if x == 0:
+        return 0
+
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end='')

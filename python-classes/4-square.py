7c0c5a5b41628ed09244a2d168065241f4987ad9
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-03 13:42:10 -0800

    python sucks at OOP

---
+++ python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""task 4"""
+
+
+class Square:
+    """The summary line for a class should fit on one line."""
+
+    def __init__(self, size=0):
+        """Initializes the instance based on args."""
+        self.size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+
+if __name__ == "__main__":
+    my_square = Square(89)
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+    my_square.size = 3
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+    try:
+        my_square.size = "5 feet"
+        print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+    except Exception as e:
+        print(e)

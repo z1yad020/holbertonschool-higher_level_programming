ff4805eacbd371cb66bd3f6c1dae78f2411031ff
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-03 08:19:02 -0800

    task0

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Square:
+    """The summary line for a class docstring should fit on one line."""
+    pass
+
+
+
+
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__doc__)

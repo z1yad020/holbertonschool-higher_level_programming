7b588295a7ab4480b9dbe5485212ab38eb3d00a8
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-03 11:37:35 -0800

    Area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""task 3"""
+
+
+class Square:
+    """The summary line for a class should fit on one line."""
+
+    def __init__(self, size=0):
+        """Initializes the instance based on args."""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+
+if __name__ == "__main__":
+    my_square_1 = Square(3)
+    print("Area: {}".format(my_square_1.area()))
+
+    try:
+        print(my_square_1.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square_1.__size)
+    except Exception as e:
+        print(e)
+
+    my_square_2 = Square(5)
+    print("Area: {}".format(my_square_2.area()))

d7a02380220b6b60c44c57321ee1927d053702a8
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-04 11:05:30 -0800

    some doctest error

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -19,12 +19,14 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
 
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix "
+                "(list of lists) of integers/floats")
 
     ln = len(matrix[0])
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix "
+                    "(list of lists) of integers/floats")
 
         if len(row) != ln:
             raise TypeError("Each row of the matrix must have the same size")
@@ -32,7 +34,8 @@ def matrix_divided(matrix, div):
         newList.append([])
         for i in row:
             if type(i) not in [int, float]:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix "
+                        "(list of lists) of integers/floats")
 
             newList[z].append(round(i / div, 2))
         z += 1

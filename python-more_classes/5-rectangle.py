1be5274f01c3bf59ba078ac1ca5fefd9543ebbb0
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-06 03:34:34 -0800

    pycode

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -75,11 +75,12 @@ class Rectangle():
 
 if __name__ == "__main__":
     my_rectangle = Rectangle(2, 4)
-    print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+    print("Area: {} - Perimeter: \
+           {}".format(my_rectangle.area(), my_rectangle.perimeter()))
 
     del my_rectangle
 
     try:
-            print(my_rectangle)
+        print(my_rectangle)
     except Exception as e:
-            print("[{}] {}".format(e.__class__.__name__, e))
+        print("[{}] {}".format(e.__class__.__name__, e))

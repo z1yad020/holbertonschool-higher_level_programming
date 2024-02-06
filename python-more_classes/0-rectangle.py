0690c0ad6a16cdce7221ab9f71ac833ae9095f18
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-06 02:28:49 -0800

    pycodestyle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+Free will Recatangle
+"""
+
 
 class Rectangle():
+    """Rectangle class """
     pass
+
 
 if __name__ == "__main__":
     my_rectangle = Rectangle()
---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Module of Rectangle
+"""
+
+
+class Rectangle():
+    """Rectangle Class"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle(2, 4)
+    print(my_rectangle.__dict__)
+
+    my_rectangle.width = 10
+    my_rectangle.height = 3
+    print(my_rectangle.__dict__)

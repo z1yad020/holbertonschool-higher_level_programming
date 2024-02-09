720c34937bb8b81f2e926f98560f9588cf821936
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-09 11:45:24 -0800

    pep8 error

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for task 3"""
+
+
+def is_kind_of_class(obj, a_class):
+    """True if the object is an instance of, or if the object is an instance
+    of a class that inherited from, the specified class ; otherwise False"""
+    return isinstance(obj, a_class)
+
+if __name__ == "__main__":
+    a = 1
+    if is_kind_of_class(a, int):
+        print("{} comes from {}".format(a, int.__name__))
+    if is_kind_of_class(a, float):
+        print("{} comes from {}".format(a, float.__name__))
+    if is_kind_of_class(a, object):
+        print("{} comes from {}".format(a, object.__name__))

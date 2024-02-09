dd114b3c173f5ecd0bfad1eb3540844a7be05e5e
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-09 12:59:35 -0800

    raising

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for task 6"""
+
+
+class BaseGeometry:
+    """
+    Base class of Geometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")

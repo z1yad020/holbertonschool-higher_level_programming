e44031623f0f88c3a517f7d81bfba0f34427d060
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-09 12:39:00 -0800

    long live bash function

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -7,6 +7,7 @@ def is_kind_of_class(obj, a_class):
     of a class that inherited from, the specified class ; otherwise False"""
     return isinstance(obj, a_class)
 
+
 if __name__ == "__main__":
     a = 1
     if is_kind_of_class(a, int):

b813c0efa4442b1ed5bc3a6f347d43c70171e9ec
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-09 14:19:27 -0800

    test

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module for task 10"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        """initilaze"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+
+if __name__ == "__main__":
+    s = Square(13)
+
+    print(s)
+    print(s.area())

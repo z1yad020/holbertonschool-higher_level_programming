624780c1e598639dc1865f511b5fa75d15112a8d
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-09 11:30:13 -0800

    exact same

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for task 2"""
+
+
+def is_same_class(obj, a_class):
+    """True if exact same, otherwise False"""
+    return type(obj) is a_class
+
+
+if __name__ == "__main__":
+    a = 1
+    if is_same_class(a, int):
+        print("{} is an instance of the class {}".format(a, int.__name__))
+    if is_same_class(a, float):
+        print("{} is an instance of the class {}".format(a, float.__name__))
+    if is_same_class(a, object):
+        print("{} is an instance of the class {}".format(a, object.__name__))

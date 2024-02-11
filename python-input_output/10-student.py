678897b982e5a2cccf533410ca0023ef081913d2
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-11 11:32:36 -0800

    task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+"""Module"""
+
+
+class Student:
+    """class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+
+        result = {}
+
+        for attr in attrs:
+            if arrt in self.__dict__:
+                result[attr] = self.__dict__[attr]
+        return result

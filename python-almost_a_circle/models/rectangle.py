520c0467db2a52c57b3a81952b24116cb2504b7f
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-14 04:28:33 -0800

    main

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -21,16 +21,3 @@ class Rectangle(Base):
 
     def __setattr__(self, name, value):
         self.__dict__[f"__{name}"] = value
-
-
-if __name__ == "__main__":
-    r1 = Rectangle(10, 2)
-    print(r1.id)
-
-    print(r1.__dict__)
-
-    r2 = Rectangle(2, 10)
-    print(r2.id)
-
-    r3 = Rectangle(10, 2, 0, 0, 12)
-    print(r3.id)

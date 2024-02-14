367f0bab9fe710ca4ad13d4e389c6a4c48d3e1bf
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-14 09:09:23 -0800

    task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,6 +17,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """JSON is one of the standard formats for sharing data representation.
         JSON string representation of list_dictionaries"""

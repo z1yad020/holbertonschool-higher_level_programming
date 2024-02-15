9bfbfbb58c09c5950f1c53706d8be8c3acc8962b
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-15 11:05:21 -0800

    try 3.

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -8,9 +8,15 @@ class testBase(unittest.TestCase):
     """test cases for Base"""
     def test_id(self):
         self.assertEqual(Base(42).id, 42)
+
+    def test_id_next(self):
         self.assertEqual(Base().id, 1)
         self.assertEqual(Base().id, 2)
+
+    def test_id_string(self):
         self.assertEqual(Base("fffff").id, "fffff")
+
+    def test_id_none(self):
         self.assertEqual(Base(None).id, 3)
 
 

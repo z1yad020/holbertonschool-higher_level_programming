7aabec016cc759a2b712aee929636f1a8f681cd6
Ziyad <ziyad.ibrahim.112358@gmail.com>
2024-02-15 10:59:33 -0800

    try 2.

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -6,14 +6,12 @@ from models.base import Base
 
 class testBase(unittest.TestCase):
     """test cases for Base"""
-    def test_id_plus(self):
+    def test_id(self):
         self.assertEqual(Base(42).id, 42)
         self.assertEqual(Base().id, 1)
         self.assertEqual(Base().id, 2)
-
-    def test_id_minus(self):
-        self.assertEqual(Base(-40).id, -40)
-        self.assertEqual(Base(-9).id, -9)
+        self.assertEqual(Base("fffff").id, "fffff")
+        self.assertEqual(Base(None).id, 3)
 
 
 if __name__ == "__main__":

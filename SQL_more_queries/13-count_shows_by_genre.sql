31736990c4743ede31534f607cf5bf1a79a50b04
Ziyad Ibrahim <ziyad.ibrahim.112358@gmail.com>
2024-03-27 19:06:41 +0400

    task 13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- numbers
+
+SELECT tv_genres.name AS `genre`, COUNT(show_id) AS `number_of_shows` FROM tv_genres
+JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY `number_of_shows` DESC;

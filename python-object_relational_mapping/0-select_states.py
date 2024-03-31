ca361fb8f3dc82dd70d76391e08e50d95d691b75
Ziyad Ibrahim <ziyad.ibrahim.112358@gmail.com>
2024-03-31 23:45:19 +0400

    task 0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+'''
+Get all states
+'''
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+
+    for r in rows:
+        print(r)

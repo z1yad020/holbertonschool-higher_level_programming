6c4ec1186409341e74e91f5070f9de9abaa85864
Ziyad Ibrahim <ziyad.ibrahim.112358@gmail.com>
2024-04-01 02:32:23 +0400

    task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+'''
+Injectionnn
+'''
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM `states` \
+                 WHERE BINARY `name` = %s ORDER BY `id`", (sys.argv[4],))
+
+    rows = cur.fetchall()
+
+    for r in rows:
+        print(r)
+
+    cur.close()
+    db.close()

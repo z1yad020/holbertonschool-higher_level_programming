a4bc8cc21deb83e558c50923ae677e378d3f5626
Ziyad Ibrahim <ziyad.ibrahim.112358@gmail.com>
2024-04-01 02:52:12 +0400

    task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+'''
+Citiesss
+'''
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT `cities.id`, `cities.name`, `states.name` FROM `cities` \
+                 JOIN `states` ON  `cities.state_id` = `states.id` \
+                 ORDER BY `cities.id`")
+
+    rows = cur.fetchall()
+    for r in rows:
+        print(r)
+
+    cur.close()
+    db.close()

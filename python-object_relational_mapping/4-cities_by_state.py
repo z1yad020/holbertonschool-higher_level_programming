2006dd0a062c0e9a51aa065b87f770ffeca9874b
Ziyad Ibrahim <ziyad.ibrahim.112358@gmail.com>
2024-04-01 02:55:23 +0400

    task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -18,9 +18,9 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("SELECT `cities.id`, `cities.name`, `states.name` FROM `cities` \
-                 JOIN `states` ON  `cities.state_id` = `states.id` \
-                 ORDER BY `cities.id`")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
+                 JOIN states ON  cities.state_id = states.id \
+                 ORDER BY cities.id")
 
     rows = cur.fetchall()
     for r in rows:

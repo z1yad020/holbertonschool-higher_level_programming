8fe9fb8acd66854d3abae1abe9d5b230326be92d
Ziyad Ibrahim <ziyad.ibrahim.112358@gmail.com>
2024-04-01 22:48:13 +0400

    task 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+Get a state
+"""
+
+import sys
+
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+if __name__ == '__main__':
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+             sys.argv[1],
+             sys.argv[2],
+             sys.argv[3]),
+             pool_pre_ping=True)
+
+    session = Session(engine)
+
+    f = session.query(State).filter(State.name.ilike(sys.argv[4]))\
+        .order_by(State.id).all()
+
+    for r in f:
+        print(r.id)
+
+    if f == []:
+        print("Not found")

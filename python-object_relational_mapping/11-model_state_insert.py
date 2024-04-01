bfda062c7805cbcdc2347e23b1ce6c67b92dfb6f
Ziyad Ibrahim <ziyad.ibrahim.112358@gmail.com>
2024-04-01 23:08:53 +0400

    task 11

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+Add a new state
+"""
+
+import sys
+
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+
+if __name__ == '__main__':
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+             sys.argv[1],
+             sys.argv[2],
+             sys.argv[3]),
+             pool_pre_ping=True)
+
+    session = Session(engine)
+
+    n = State(name='Louisiana')
+    session.add(n)
+    session.commit()
+
+    print(n.id)
